29b21044c06ea2a42be83d3e00d8e8056537d86d
Felipe Silva <felipyago@outlook.com>
2025-05-17 09:38:06 -0300

    Precedencias

---
+++ src/OrdemPrecedencia/Precedencia.java
@@ -0,0 +1,19 @@
+package OrdemPrecedencia;
+
+public class Precedencia {
+    public static void main(String[] args) {
+
+        boolean a = true;
+        boolean b = false;
+        boolean c = true;
+
+        boolean resultado = a && b || c;
+        System.out.println(resultado);
+
+        boolean resultado2 = (a && b) || c;
+        System.out.println(resultado2);
+
+        boolean resultado3 = a && !(b || c);
+        System.out.println(resultado3);
+    }
+}
